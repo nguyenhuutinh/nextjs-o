53c604095c3692e2238e9a55b756317c954876ce
tinhnguyen <tn@ipification>
2021-02-23 18:48:03 +0700

    update

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Green O- Sinh Tố Rau Sạch</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
