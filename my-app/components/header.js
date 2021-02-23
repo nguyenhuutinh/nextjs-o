a4cb487133d8fd42deb6c3d2eaa3723089cefd94
tinhnguyen <tn@ipification>
2021-02-23 23:20:04 +0700

    fix bug

--- my-app/components/header.js
+++ my-app/components/header.js
@@ -8,20 +8,27 @@ const Header = () => {
 
 	
 	return (
-		<div className={classes.header}>
-			<div className={classes.left}>
-                <LazyLoadImage effect="blur"   className={classes.logo} src="../images/greeno-logo.png" placeholderSrc={"../images/greeno-logo-pre.png"}/>
-                <div className={classes.slogan}>
-                Thải Độc - Giảm Cân - Đẹp Da
+		<div>
+            <div className={classes.topbar}>
+                
+                <div>416A1 Hai Bà Trưng, Phường Tân Định, Quận 1, TP. Hồ Chí Minh</div>
+                <div>Hotline: 1900 63 67 91 - 0909 09 60 60 - 0909 44 80 60</div>
+            </div>
+            <div className={classes.header}>
+                <div className={classes.left}>
+                    <LazyLoadImage effect="blur"   className={classes.logo} src="../images/greeno-logo.png" placeholderSrc={"../images/greeno-logo-pre.png"}/>
+                    <div className={classes.slogan}>
+                    Thải Độc - Giảm Cân - Đẹp Da
+                    </div>
                 </div>
-            </div>
-            <div className={classes.right}>
-                <div className={classes.link_item}><a href="/thuc-don-le"> Sản Phẩm</a></div>
-                <div className={classes.link_item}><a href="/thuc-don-giam-can"> Gói Định Kỳ</a></div>
-                <div className={classes.link_item}><a href="/thong-tin-dinh-duong">Thông tin dinh dưỡng</a></div>
-                <div className={classes.link_item}><a href="/cau-hoi-thuong-gap">Câu Hỏi Thường Gặp</a></div>
-            </div>
-		</div>
+                <div className={classes.right}>
+                    <div className={classes.link_item}><a href="https://www.greeno.vn/thuc-don-le"> Sản Phẩm</a></div>
+                    <div className={classes.link_item}><a href="https://www.greeno.vn/thuc-don-giam-can"> Gói Định Kỳ</a></div>
+                    <div className={classes.link_item}><a href="https://www.greeno.vn/thong-tin-dinh-duong">Thông tin dinh dưỡng</a></div>
+                    <div className={classes.link_item}><a href="https://www.greeno.vn/cau-hoi-thuong-gap">Câu Hỏi Thường Gặp</a></div>
+                </div>
+		    </div>
+        </div>
 	);
 };
 
