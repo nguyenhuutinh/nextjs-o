159ae485ffdfaf9998c414db2123c2d0f7fc1d94
tinhnguyen <tn@ipification>
2021-02-24 14:46:54 +0700

    Update promotion7days.js

--- my-app/containers/promotion7days.js
+++ my-app/containers/promotion7days.js
@@ -10,7 +10,7 @@ const Promotion7Days = ({callback}) => {
 						<div className={styles.left} >
 			<div className={styles.text1}>Giải Pháp Hỗ Trợ Giảm Cân An Toàn </div>
               <br/>
-              <div className={styles.text2}>Trong 7 - 14 Ngày</div>
+              <div className={styles.text2}>Từ 7 - 14 Ngày</div>
               <br/>
               <div className={styles.text3}>Cùng Green O</div>
             </div>
