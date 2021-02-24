db425bd5206568c09aa2ff45226b0e96085aca2e
tinhnguyen <tn@ipification>
2021-02-24 14:38:08 +0700

    fix bug

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -79,11 +79,20 @@ export default function Home() {
 					}}
 				/>
 			</Modal>
-		<Button type="primary">aaaaa</Button>
+	
 			</main>
 
 			<footer className={styles.footer}>
-			
+			<LazyLoadImage src="../images/greeno-logo.png" className={styles.footer_logo} effect={"blur"}/>
+			<div className={styles.footer_content}><b style={{color:'#185c2b'}}>Green O - Sinh Tố Rau Sạch</b> là thương hiệu thức uống chú trọng đến các tiêu chí tươi, sạch, an toàn của nguyên liệu sử dụng nhằm đem lại lợi ích tốt nhất cho làn da và vóc dáng của bạn. Tự hào đem đến những sản phẩm an toàn và hiệu quả cao, góp phần tích cực trong việc cải thiện sắc vóc và sức khỏe người tiêu dùng Việt.
+			<br/>
+			<br/><br/>
+			Cửa hàng chính: 416A1 Hai Bà Trưng, Phường Tân Định, Quận 1, TP. Hồ Chí Minh<br/>
+			Chi Nhánh Hà Nội: Toà nhà Việt Đức Complex, 39 Lê Văn Lương, Phường Nhân Chính, Quận Thanh Xuân, Thành Phố Hà Nội<br/>
+			Hotline: 1900 63 67 91 - 090909 60 60 - 090944 80 60<br/>
+			Email: vietnam@thousandhands.com<br/>
+				</div>
+				<div className={styles.trademak}>Copyright © 2021 Green O - Sinh Tố Rau Sạch. All Rights Reserved.</div>
 			</footer>
 		</div>
 	);
