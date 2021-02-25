57030a690200fe65df575cb6c5e6572ba80d2706
tinhnguyen <tn@ipification>
2021-02-26 00:20:15 +0700

    fix bug

--- my-app/components/header.js
+++ my-app/components/header.js
@@ -6,7 +6,25 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 const Header = () => {
 	
 	useEffect(() => {}, []);
-
+    const height = 20;
+    const [isScroll, setScroll] = useState(false);
+    useEffect(() => {
+      window.onscroll = function(e) {
+        {
+          if (window.pageYOffset > height) {
+            setScroll(true);
+          } else {
+            setScroll(false);
+          }
+        }
+      };
+      return () => {
+        // console.log('Cleaned up');
+        setScroll(false);
+        window.onscroll = null;
+      };
+    }, []);
+    var xx = isScroll ? `${height - window.pageYOffset> 0 ? height - window.pageYOffset : 0}px` : `${height}px`
 	
 	return (
 		<div>
@@ -15,7 +33,7 @@ const Header = () => {
                 <div>416A1 Hai Bà Trưng, Phường Tân Định, Quận 1, TP. Hồ Chí Minh</div>
                 <div>Hotline: 1900 63 67 91 - 0909 09 60 60 - 0909 44 80 60</div>
             </div>
-            <div className={classes.header}>
+            <div className={classes.header} style={{top: xx}}>
                 <div className={classes.left}>
                     <Link href="/" style={{cursor:"pointers"}}><LazyLoadImage effect="blur"   className={classes.logo} src="../images/greeno-logo.png" /></Link>
                     <div className={classes.slogan}>
