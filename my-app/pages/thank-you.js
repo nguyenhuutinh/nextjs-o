f3f201538731af613a72841b1fd21fde3bbd190c
tinhnguyen <tn@ipification>
2021-02-26 10:42:58 +0700

    fix css

--- my-app/pages/thank-you.js
+++ my-app/pages/thank-you.js
@@ -25,7 +25,7 @@ export default function Home() {
 				<Header />
             
 			<Card className={styles.thankyou_card}>
-			<br/><br/>
+			
 			<LazyLoadImage src="../images/greeno-logo.png" className={styles.thankyou_logo} effect={'blur'} />
                     <div className={styles.thankyou_text}>Cảm ơn quý khách đã liên hệ. <br/>Bộ Phận CSKH của Green O sẽ liên lạc lại trong thời gian sớm nhất
 					<br/><br/>Hotline: 1900 636791
