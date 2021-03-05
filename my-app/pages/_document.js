594b3fa77d2b2f0f9a6ceb1e1437c5fb76e04ed0
tinhnguyen <tn@ipification>
2021-03-05 11:45:49 +0700

    update gtm

--- my-app/pages/_document.js
+++ my-app/pages/_document.js
@@ -12,6 +12,26 @@ class MyDocument extends Document {
         
 
         <Head>
+        <link rel="dns-prefetch" href="//www.googletagmanager.com"></link>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-5G5638H`}
+          />
+          
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function(w, d, s, l, i){
+                  w[l] = w[l] || []; w[l].push({
+                      'gtm.start':
+                          new Date().getTime(), event: 'gtm.js'
+                  }); var f = d.getElementsByTagName(s)[0],
+                      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+                          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+                }) (window, document, 'script', 'dataLayer', 'GTM-5G5638H');
+            `,
+              }}
+          />
         <meta name="google" content="notranslate"/>
         </Head>
         <body class="notranslate">
