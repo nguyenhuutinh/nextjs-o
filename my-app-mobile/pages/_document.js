f0077552cf6d0184df531bf442913da824761b4f
tinhnguyen <tn@ipification>
2021-05-10 12:04:49 +0700

    add puzz

--- my-app-mobile/pages/_document.js
+++ my-app-mobile/pages/_document.js
@@ -33,6 +33,21 @@ class MyDocument extends Document {
             `,
               }}
           />
+          <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                (function (i, s, o, g, r, a, m) {
+                  i['MePuzzAnalyticsObject'] = r; i[r] = i[r] || function () {
+                      (i[r].q = i[r].q || []).push(arguments);
+                  }, i[r].l = 1 * new Date(); a = s.createElement(o),
+                      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g;
+                  m.parentNode.insertBefore(a, m);
+                  })(window, document, 'script', 'https://static.mepuzz.com/sdk/v2/MePuzzSDK.min.js', 'mePuzz');
+                  mePuzz('setAppId', 'BnZenzGejJ');
+                  mePuzz('track','pageview');
+            `,
+              }}
+          />
         </Head>
         <body class="notranslate">
           <Main />
